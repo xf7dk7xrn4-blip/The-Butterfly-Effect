97932df9971e6992524c5f959e82516b037c4e29
xf7dk7xrn4-blip <xf7dk7xrn4@privaterelay.appleid.com>
2026-03-11 16:28:01 +0100

    Delete The ButterFly Effect directory

--- The ButterFly Effect/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { EventSelection } from './components/EventSelection';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-900 text-slate-50">
-      <EventSelection />
-    </div>
-  );
-}
